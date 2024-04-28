246f5ed76a0bce8d09ca84cb6bcff96e94925ff8
Subhradeep Chakraborty <chakrabortysubhradeep556@gmail.com>
2024-04-28 20:14:25 +0530

    fix: GET rbac permission for settings

--- src/common/utils/auth.rs
+++ src/common/utils/auth.rs
@@ -197,7 +197,7 @@ impl FromRequest for AuthExtractor {
                 path_columns[0].to_string()
             }
         } else if url_len == 2 || (url_len > 2 && path_columns[1].starts_with("settings")) {
-            if method.eq("GET") {
+            if method.eq("GET") && !path_columns[1].starts_with("settings") {
                 method = "LIST".to_string();
             }
             format!(
